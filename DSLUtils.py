b422bc9478a786115e0570ab91c80998fa532e20
Daniel Pereira <81535464+danielrspereira@users.noreply.github.com>
2021-04-05 12:18:08 +0200

    Update DSLUtils.py
    Exception as a workaround for connection error

--- DSLUtils.py
+++ DSLUtils.py
@@ -6,11 +6,17 @@ from datetime import datetime
 class Dsl:
     def record_dsl_speed(self, file):
         df = pd.read_excel(file, sheet_name='base')
-        print(dir(speedtest))
-        s = speedtest.Speedtest()
-        s.get_best_server()
-        download = s.download(threads=None) * (10 ** -6)
-        upload = s.upload(threads=None) * (10 ** -6)
+        download = 0.0
+        upload = 0.0
+        # in case of connection problem this function will deliver speed = 0
+        try:
+            s = speedtest.Speedtest()
+            s.get_best_server()
+            download = s.download(threads=None) * (10 ** -6)
+            upload = s.upload(threads=None) * (10 ** -6)
+        
+        except:
+            pass
         now = datetime.now()
         date = now.strftime("%m/%d/%Y")
         hour = now.strftime("%H:%M:%S")
